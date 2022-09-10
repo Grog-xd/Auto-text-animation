6299b5b3fb6f4a31cad28310ee3eaa380997e97b
Grog <groglul@gmail.com>
2022-09-10 14:46:28 +0300

    Added not infinite animation

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Auto-text-animation
 ## How it works?
 #### Just enter the attribute in the text tag: `data-speed` with a speed in ms.
+#### You can also add `data-infinity='true'` attribute for infinite animation.
 #### Don't forget to connect the script to html.
 ## Example:
+### Default:
 ```html
   <h1 data-speed="500">Hellow world</h1>
+```
+### Infinity:
+```html
+  <h1 data-speed="500" data-infinity='true'>Hellow world</h1>
 ```
--- auto-text-animation.js
+++ auto-text-animation.js
@@ -1,23 +1,30 @@
 const texts = document.querySelectorAll('[data-speed]')
 
 
-texts.forEach(text =>{
+
+texts.forEach(text =>{ 
+
     let str = text.textContent
     if(!str){
-        console.log('пожалуйста используйте данный скрипт только с текстовыми тегами')
-        return
+        throw new Error('Please use this script with text tags only')
     }
     let result = ''
     let i = 0
     text.innerHTML = ''
 
-    setInterval(() => {
+    let interval = setInterval(() => {
         result += str[i]
         i++
         text.innerHTML = result
         if(i == str.length){
-            result = ''
-            i = 0
+            if(text.dataset.infinity == 'true'){
+                result = ''
+                i = 0
+            }
+            else{
+                clearInterval(interval)
+            }
+            
         }
     }, text.dataset.speed);
 })
